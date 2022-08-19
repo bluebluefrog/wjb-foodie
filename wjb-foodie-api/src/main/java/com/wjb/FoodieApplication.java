7e36d111ae0409bc504cf1b285592722bfea0a4a
jingbin.wang <79555784+bluebluefrog@users.noreply.github.com>
2022-08-20 01:21:11 +1000

    init

--- wjb-foodie-api/src/main/java/com/wjb/FoodieApplication.java
+++ wjb-foodie-api/src/main/java/com/wjb/FoodieApplication.java
@@ -1,9 +1,11 @@
 package com.wjb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.wjb.mapper")
 public class FoodieApplication {
 
     public static void main(String[] args) {
