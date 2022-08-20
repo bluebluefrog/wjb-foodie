76710b89df8d3197193b0fcf52a229528fa57975
jingbin.wang <79555784+bluebluefrog@users.noreply.github.com>
2022-08-20 15:57:07 +1000

    init

---
+++ wjb-foodie-mapper/src/main/java/com/wjb/mapper/FoodieUserMapper.java
@@ -0,0 +1,7 @@
+package com.wjb.mapper;
+
+import com.wjb.my.mapper.MyMapper;
+import com.wjb.pojo.FoodieUser;
+
+public interface FoodieUserMapper extends MyMapper<FoodieUser> {
+}
--- wjb-foodie-mapper/src/main/java/com/wjb/mapper/UsersMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wjb.mapper;
-
-import com.wjb.my.mapper.MyMapper;
-import com.wjb.pojo.Users;
-
-public interface UsersMapper extends MyMapper<Users> {
-}
